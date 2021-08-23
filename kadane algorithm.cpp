781e1124318e1bc6d411917ebc5705521ebef085
Param Jani <69115040+paramjani12@users.noreply.github.com>
2021-08-23 12:17:23 +0530

    Create kadane algorithm.cpp

---
+++ kadane algorithm.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int currSum=0;
+    int maxSum=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        currSum+=arr[i];
+        if(currSum<0)
+        {
+            currSum=0;
+        }
+        maxSum=max(maxSum,currSum);
+    }
+    /* or 2nd way
+    for(int i=0;i<n;i++)
+    {
+        if(currSum>=0)
+        {
+            currSum+=arr[i];
+        }
+        else
+        {
+            currSum=arr[i];
+        }
+        maxSum=max(maxSum,currSum);
+    }*/
+    cout<<maxSum;
+    return 0;
+}
+
+
