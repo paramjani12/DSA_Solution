86fc1df065b08db9ebf8e7859695088026d68605
Param Jani <69115040+paramjani12@users.noreply.github.com>
2021-08-23 12:20:00 +0530

    Create cumulative sum approach or prefix sum approach case-1(wrong output).cpp

---
+++ cumulative sum approach or prefix sum approach case-1(wrong output).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int mx=INT_MIN;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int currsum[n];
+    currsum[0]=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        currsum[i]=currsum[i-1]+arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=0;j<i;j++)
+        {
+            sum=currsum[i]-currsum[j];
+            mx=max(mx,sum);
+        }
+    }
+    cout<<mx<<endl;
+    return 0;
+}
